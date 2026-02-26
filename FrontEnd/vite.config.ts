8d5d1e48e2554641981a93391f595ef1de8b34ae
tonywahome <a.wambugu@alustudent.com>
2026-02-26 20:49:46 +0200

    nafsi deploy

--- FrontEnd/vite.config.ts
+++ FrontEnd/vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "nafsi_health_deploy",
+  base: "/nafsi_health_deploy/",
+  server: {
+    port: 5173,
+    strictPort: false,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
 });
